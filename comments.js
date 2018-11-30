eac59a9d422201ca4e5f4d692c481693e005ab51
Damian Godek <damian.godek1@gmail.com>
2018-11-30 22:09:50 +0100

    add object.asign method

--- comments.js
+++ comments.js
@@ -1,25 +1,21 @@
-import {CREATE_COMMENT} from './actions';
-import {REMOVE_COMMENT} from './actions';
-import {EDIT_COMMENT} from './actions';
-import {THUMB_UP_COMMENT} from './actions';
-import {THUMB_DOWN_COMMENT} from './actions';
+import { CREATE_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_DOWN_COMMENT, THUMB_UP_COMMENT } from './actions'
 
 const initialState = {
     comments: [],
     users: []
 };
 
-function comments(state = [], action) {
+function comments(state = initialState, action) {
     switch(action.type) {
         case CREATE_COMMENT:
-            return [{
+            return Objetct.assign({}, state, { [{
                 id: action.id,
                 text: action.text,
                 votes: 0,
             }
-            , ...state];
+            , ...state]});
         case REMOVE_COMMENT:
-            return state.filter(comment => comment.id !== action.id);
+            return Object.assign({}, state, { state.filter(comment => comment.id !== action.id)});
         case EDIT_COMMENT:
             return state.map(comment => {
                 if (comment.id === action.id) {
@@ -34,7 +30,7 @@ function comments(state = [], action) {
                 }
                 return comment;
             });
-        case HUMB_UP_COMMENT:
+        case THUMB_UP_COMMENT:
             return state.map(comment => {
                 if(comment.id === action.id) {
                     return {...comment, votes: comment.votes + 1}
