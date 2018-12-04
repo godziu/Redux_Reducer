d771ddba965b363472e2d966b5d34689bced3f3a
Damian Godek <damian.godek1@gmail.com>
2018-12-04 12:09:17 +0100

    change comments.js

--- comments.js
+++ comments.js
@@ -1,46 +1,46 @@
-import { CREATE_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_DOWN_COMMENT, THUMB_UP_COMMENT } from './actions'
+import { ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_DOWN_COMMENT, THUMB_UP_COMMENT } from './actions';
 
-const initialState = {
-    comments: [],
-    users: []
-};
-
-function comments(state = initialState, action) {
-    switch(action.type) {
-        case CREATE_COMMENT:
-            return Objetct.assign({}, state, { [{
-                id: action.id,
-                text: action.text,
-                votes: 0,
-            }
-            , ...state]});
-        case REMOVE_COMMENT:
-            return Object.assign({}, state, { state.filter(comment => comment.id !== action.id)});
-        case EDIT_COMMENT:
-            return state.map(comment => {
-                if (comment.id === action.id) {
-                    return {...comment, text: action.text}
-                }
-                return comment;
-            });
-        case THUMB_DOWN_COMMENT:
-            return state.map(comment => {
-                if (comment.id === action.id) {
-                    return {...comment, votes: comment.votes - 1}
-                }
-                return comment;
-            });
-        case THUMB_UP_COMMENT:
-            return state.map(comment => {
-                if(comment.id === action.id) {
-                    return {...comment, votes: comment.votes + 1}
-                }
-                return comment;
-            });
-        default:
-            return state;
-
-    }
+export function comments(state = [], action) {
+  switch (action.type) {
+    case ADD_COMMENT:
+      return Object.assign({}, state, {
+        comments: [{
+          id: action.id,
+          text: action.text,
+          votes: 0
+        }, ...state]
+      });
+    case REMOVE_COMMENT:
+      return Object.assign({}, state, {
+        comments: state.comments.filter(comment => comment.id !== action.id)
+      });
+    case EDIT_COMMENT:
+      return Object.assign({}, state, {
+        comments: state.comments.map(comment => {
+          if (comment.id === action.id) {
+            comment.text = action.text;
+          }
+        })
+      });
+    case THUMB_UP_COMMENT:
+      return Object.assign({}, state, {
+        comments: state.comments.map(comment => {
+          if (comment.id === action.id) {
+            comment.vote = action.vote + 1;
+          }
+        })
+      });
+    case THUMB_DOWN_COMMENT:
+      return Object.assign({}, state, {
+        comments: state.comments.map(comment => {
+          if (comment.id === action.id) {
+            comment.vote = action.vote - 1;
+          }
+        })
+      });
+    default:
+      return state;
+  }
 }
 
 export default comments;
